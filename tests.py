2447e2c3ebe69a5c6e60920970ba931c37fac1bd
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2024-08-15 07:46:58 +0200

    Make test work across versions.
    Better to rewrite the test, but this at least fixes it.
    One should check that no implementation-dependent code is used in the solvers.

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import sys
 from dsl import *
 
 
@@ -306,7 +307,15 @@ def test_papply():
  
 
 def test_mpapply():
-    assert mpapply(lambda x, y: frozenset({(x, (i, j)) for _, (i, j) in y}), (3, 4), frozenset({frozenset({(1, (0, 0))}), frozenset({(1, (1, 1)), (1, (0, 1))})})) == ((3, (0, 1)), (3, (1, 1)), (4, (0, 0)))
+    if sys.version_info >= (3, 8):
+        # Code for Python 3.8 and later
+        assert mpapply(lambda x, y: frozenset({(x, (i, j)) for _, (i, j) in y}), (3, 4), frozenset({frozenset({(1, (0, 0))}), frozenset({(1, (1, 1)), (1, (0, 1))})})) == ((3, (0, 1)), (3, (1, 1)), (4, (0, 0)))
+    else:
+        # Code for Python 3.7 and earlier
+        assert mpapply(lambda x, y: frozenset({(x, (i, j)) for _, (i, j) in y}), (3, 4), frozenset({frozenset({(1, (0, 0))}), frozenset({(1, (1, 1)), (1, (0, 1))})})) == ((3, (0, 0)), (4, (1, 1)), (4, (0, 1)))
+
+
+
  
 
 def test_prapply():
